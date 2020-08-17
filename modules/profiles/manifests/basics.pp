329b25378ad0afa919af003234c370c30d19b893
Tim Meusel <tim@bastelfreak.de>
2020-08-17 23:27:30 +0200

    simulate pluginsync

--- modules/profiles/manifests/basics.pp
+++ modules/profiles/manifests/basics.pp
@@ -1,4 +1,17 @@
 class profiles::basics {
+
+  # do a pluginsync in agentless setup
+  # lint:ignore:puppet_url_without_modules
+  file { $::settings::libdir:
+    ensure  => directory,
+    source  => 'puppet:///plugins',
+    recurse => true,
+    purge   => true,
+    backup  => false,
+    noop    => false,
+  }
+  # lint:endignore
+
   package { ['make', 'gcc', 'build-essential']:
     ensure => 'installed',
   }
