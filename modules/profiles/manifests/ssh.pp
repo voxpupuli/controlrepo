f0b33439340f798b0446f20bac142385ec77db0b
Tim Meusel <tim@bastelfreak.de>
2020-08-18 08:11:15 +0200

    setup ssh key for github

--- modules/profiles/manifests/ssh.pp
+++ modules/profiles/manifests/ssh.pp
@@ -5,4 +5,26 @@
 #
 class profiles::ssh {
   contain ssh
+
+  # /root/.ssh/config entry to access github.com fast
+  ssh::client::config::user { 'root_github':
+    ensure        => present,
+    user          => 'root',
+    user_home_dir => '/root',
+    options       => {
+      'Host github.secureserver.net' => {
+        'User'           => 'git',
+        'IdentityFile'   => '~/.ssh/id_ed25519_github',
+        'ControlMaster'  => 'auto',
+        'ControlPath'    => '~/.ssh/ssh-%r@%h:%p',
+        'ControlPersist' => '10m',
+      },
+    },
+  }
+  # setup ssh key exchange
+  ssh_keygen { 'root_github':
+    type     => 'ed25519',
+    user     => 'root',
+    filename => "/root/.ssh/id_ed25519_github",
+  }
 }
