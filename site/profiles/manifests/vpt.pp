971e474595fbe7f701e5f7b2d1a5311da5be7309
Tim Meusel <tim@bastelfreak.de>
2022-03-18 08:40:31 +0100

    profiles::vpt: Add profile

---
+++ site/profiles/manifests/vpt.pp
@@ -0,0 +1,10 @@
+#
+# @summary this profile will, in the future, instal Vox Pupuli Tasks
+#
+# @see https://github.com/voxpupuli/vox-pupuli-tasks
+#
+# @author Tim Meusel <tim@bastelfreak.de>
+#
+class profiles::vpt {
+  require profiles::nginx
+}
