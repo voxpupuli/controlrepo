758f0286d77c3f0a9850884cd92281e33fc2b292
Tim Meusel <tim@bastelfreak.de>
2022-12-21 10:58:19 +0100

    Implement sshd management

--- site/profiles/manifests/base.pp
+++ site/profiles/manifests/base.pp
@@ -15,4 +15,27 @@ class profiles::base {
   package { 'snapd':
     ensure => 'absent',
   }
+  # https://www.sshaudit.com/hardening_guides.html
+  class { 'ssh':
+    storeconfigs_enabled => false,
+    validate_sshd_file   => true,
+    server_options       => {
+      'PasswordAuthentication' => 'no',
+      'PermitRootLogin'        => 'without-password',
+      'X11Forwarding'          => 'no',
+      'PrintMotd'              => 'yes',
+      'AllowAgentForwarding'   => 'no',
+      'Protocol'               => 2,
+      'Port'                   => 22,
+    },
+    client_options       => {
+      'Host *' => {
+        'Ciphers'           => 'chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr',
+        'KexAlgorithms'     => 'curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256',
+        'MACs'              => 'hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com',
+        'HostKeyAlgorithms' => 'ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,sk-ssh-ed25519@openssh.com,sk-ssh-ed25519-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-256-cert-v01@openssh.com,rsa-sha2-512,rsa-sha2-512-cert-v01@openssh.com',
+      },
+    },
+  }
+  contain ssh
 }
