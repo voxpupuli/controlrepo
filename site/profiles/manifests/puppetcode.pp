e2e94a556b3c16bdab70dc794382a4621e9be4cd
Tim Meusel <tim@bastelfreak.de>
2022-12-21 11:47:14 +0100

    Configure ssh access to GitHub

--- site/profiles/manifests/puppetcode.pp
+++ site/profiles/manifests/puppetcode.pp
@@ -4,6 +4,27 @@
 # @author Tim Meusel <tim@bastelfreak.de>
 #
 class profiles::puppetcode {
+  ssh_keygen { 'root_github':
+    type     => 'ed25519',
+    filename => '/root/.ssh/id_ed25519_github',
+    home     => '/root',
+    user     => 'root',
+  }
+  # /root/.ssh/config entry for the backup server
+  ssh::client::config::user { 'root_github':
+    ensure        => present,
+    user          => 'root',
+    user_home_dir => '/root',
+    options       => {
+      'Host github.com' => {
+        'User'           => 'git',
+        'IdentityFile'   => '~/.ssh/id_ed25519_github',
+        'ControlMaster'  => 'auto',
+        'ControlPath'    => '~/.ssh/ssh-%r@%h:%p',
+        'ControlPersist' => 'yes',
+      },
+    },
+  }
   if $facts['os']['name'] == 'Archlinux' {
     $deploy = { 'generate_types' => true, 'puppet_path' => '/usr/bin/puppet' }
     $version = 'installed'
--- site/profiles/spec/classes/puppetcode_spec.rb
+++ site/profiles/spec/classes/puppetcode_spec.rb
@@ -11,6 +11,9 @@ describe 'profiles::puppetcode' do
 
       context 'with all defaults' do
         it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('r10k') }
+        it { is_expected.to contain_ssh_keygen('root_github') }
+        it { is_expected.to contain_ssh__client__config__user('root_github') }
       end
     end
   end
