57a0ad379016f20e26e8eca8d5119ad618ba7dd4
Tim Meusel <tim@bastelfreak.de>
2023-07-27 22:30:41 +0200

    dont install gcc twice

--- site/profiles/manifests/puppet.pp
+++ site/profiles/manifests/puppet.pp
@@ -23,7 +23,7 @@ class profiles::puppet (
       server_jvm_extra_args      => ['-Djruby.logger.class=com.puppetlabs.jruby_utils.jruby.Slf4jLogger', '-XX:+UseParallelGC'],
       server_multithreaded       => true,
     }
-    package { ['gcc', 'pgbadger', 'pg_activity']:
+    package { ['pgbadger', 'pg_activity']:
       ensure => 'installed',
     }
     package { 'msgpack-server':
