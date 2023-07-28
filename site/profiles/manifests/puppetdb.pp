a27a1379d9ac9996c525f189492d54fd01d1c5c5
Tim Meusel <tim@bastelfreak.de>
2023-07-28 16:38:56 +0200

    foo

--- site/profiles/manifests/puppetdb.pp
+++ site/profiles/manifests/puppetdb.pp
@@ -5,6 +5,7 @@ class profiles::puppetdb {
   require profiles::postgresql
   class { 'puppetdb':
     manage_dbserver => false,
+    manage_firewall => false,
   }
   contain puppetdb
   class { 'puppetdb::master::config':
