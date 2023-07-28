7db7d57faa7ea5baec373e642bb16d48a3b72cd9
Tim Meusel <tim@bastelfreak.de>
2023-07-28 13:41:50 +0200

    fix pluginsync

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,19 @@
+# hack pluginsync as file resource. only required for `puppet apply` usage
+# this works by accident with puppet agent, but only on the puppetserver
+# it breaks puppet agent on other systems, so we need to guard it
+if $trusted['authenticated'] == 'local' {
+  file { $settings::libdir:
+    ensure  => directory,
+    source  => 'puppet:///plugins', # lint:ignore:puppet_url_without_modules
+    recurse => true,
+    purge   => true,
+    backup  => false,
+    noop    => false,
+  }
+}
+
 # include base profile that every node gets
 contain profiles::base
-
-## pluginsync
-file { $::settings::libdir: # lint:ignore:top_scope_facts
-  ensure  => directory,
-  source  => 'puppet:///plugins', # lint:ignore:puppet_url_without_modules
-  recurse => true,
-  purge   => true,
-  backup  => false,
-  noop    => false,
-}
 
 # include node specific profiles
 lookup('classes', Array[String[1]], 'unique', []).each |$c| {
