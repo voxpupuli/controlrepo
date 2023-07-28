27636d8859e4d9cf4452641d941813a043903a69
Tim Meusel <tim@bastelfreak.de>
2023-07-28 13:41:50 +0200

    fix pluginsync

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,18 @@
-## pluginsync
-file { $::settings::libdir: # lint:ignore:top_scope_facts
-  ensure  => directory,
-  source  => 'puppet:///plugins', # lint:ignore:puppet_url_without_modules
-  recurse => true,
-  purge   => true,
-  backup  => false,
-  noop    => false,
+# hack pluginsync as file resource. only required for `puppet apply` usage
+# this works by accident with puppet agent, but only on the puppetserver
+# it breaks puppet agent on other systems, so we need to guard it
+if $trusted['authenticated'] == 'local' {
+  file { $settings::libdir:
+    ensure  => directory,
+    source  => 'puppet:///plugins', # lint:ignore:puppet_url_without_modules
+    recurse => true,
+    purge   => true,
+    backup  => false,
+    noop    => false,
+  }
 }
 
-# make sure EVERY node gets baseline settings, even if it doesn't yet have a role
+# include base profile that every node gets
 contain profiles::base
 
 # Look in Hiera for the role to be included. This method will only permit a
@@ -16,4 +20,9 @@ contain profiles::base
 $_node_role = lookup('role', String, 'first', '')
 unless empty($_node_role) {
   include($_node_role)
+}
+
+# include node specific profiles
+lookup('classes', Array[String[1]], 'unique', []).each |$c| {
+  contain $c
 }
