ecee81cccf85a46448554076708c11fcadd76083
Tim Meusel <tim@bastelfreak.de>
2024-08-16 20:37:31 +0200

    Install dfc

--- site/profiles/manifests/base.pp
+++ site/profiles/manifests/base.pp
@@ -8,7 +8,7 @@
 class profiles::base (
   Boolean $manage_borg = true,
 ) {
-  package { ['make', 'gcc', 'build-essential', 'htop', 'lsb-release', 'ctop', 'ca-certificates', 'apt-file']:
+  package { ['make', 'gcc', 'build-essential', 'htop', 'lsb-release', 'ctop', 'ca-certificates', 'apt-file', 'dfc']:
     ensure => 'installed',
   }
   exec { 'refresh apt-file cache':
