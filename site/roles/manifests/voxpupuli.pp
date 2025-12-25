5c92a61d70373a3404b4d7221594784d51a94873
Tim Meusel <tim@bastelfreak.de>
2025-12-25 19:22:43 +0100

    fix ordering

--- site/roles/manifests/voxpupuli.pp
+++ site/roles/manifests/voxpupuli.pp
@@ -2,10 +2,10 @@
 # This role encompasses the various applications that makeup the host at voxpupu.li
 #
 class roles::voxpupuli {
+  include profiles::prometheus
   include profiles::grafana
   include profiles::node_exporter
   include profiles::postgres_exporter
-  include profiles::prometheus
   include profiles::puppetcode
   include profiles::puppetmodule
   include profiles::vpt
