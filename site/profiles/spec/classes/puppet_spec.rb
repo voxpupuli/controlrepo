2503fda083e999fb282078a6a5d772defb501c3d
Tim Meusel <tim@bastelfreak.de>
2025-12-25 21:59:15 +0100

    fix tests

--- site/profiles/spec/classes/puppet_spec.rb
+++ site/profiles/spec/classes/puppet_spec.rb
@@ -11,9 +11,9 @@ describe 'profiles::puppet' do
 
       context 'with all defaults' do
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to contain_class('r10k') }
-        it { is_expected.to contain_ssh_keygen('root_github') }
-        it { is_expected.to contain_ssh__client__config__user('root_github') }
+        it { is_expected.not_to contain_class('r10k') }
+        it { is_expected.not_to contain_ssh_keygen('root_github') }
+        it { is_expected.not_to contain_ssh__client__config__user('root_github') }
       end
     end
   end
