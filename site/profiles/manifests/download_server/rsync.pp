257a940f40f069f40c7a303892b3dc2d2d1e374a
Tim Meusel <tim@bastelfreak.de>
2026-01-24 16:58:25 +0100

    mirror: exclude lost+found

--- site/profiles/manifests/download_server/rsync.pp
+++ site/profiles/manifests/download_server/rsync.pp
@@ -30,7 +30,7 @@ class profiles::download_server::rsync {
     ;
     'all':
       comment => 'Vox Pupuli Artifacts, Downloads, and Repositories',
-      exclude => ['/yum/lost+found/', '/apt/lost+found/'],
+      exclude => ['/artifacts/lost+found/', '/yum/lost+found/', '/apt/lost+found/'],
     ;
     'packages':
       comment => 'Vox Pupuli Downloads and Repositories',
