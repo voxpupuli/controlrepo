cc07e794915fcc6c222bd250bf9a6ee948feaaa0
Tim Meusel <tim@bastelfreak.de>
2026-08-26 20:04:59 +0200

    Foreman: Update 3.18->3.19

--- site/profiles/manifests/foreman.pp
+++ site/profiles/manifests/foreman.pp
@@ -9,7 +9,7 @@ class profiles::foreman {
   require profiles::nftables # ensures hkp access is working to download the apt key
 
   class { 'foreman::repo':
-    repo => '3.18',
+    repo => '3.19',
   }
 
   class { 'foreman':
